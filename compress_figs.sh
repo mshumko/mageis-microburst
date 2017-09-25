3425c6ee27caddf8d514cad1694f64fc4138b8e7
Mike Shumko <msshumko@gmail.com>
2017-09-25 13:50:41 -0600

    Tested the velocity->momentum transformation. It works. The issue is in the resonance curve equation...

--- compress_figs.sh
+++ compress_figs.sh
@@ -2,6 +2,6 @@
 
 # This script will compress figure 1 and 2, and replace the files.
 
-gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -sOutputFile=fig1_compressed.pdf fig1.pdf
+gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -sOutputFile=figure_scripts/fig1_compressed.pdf figure_scripts/fig1.pdf
 
-gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -sOutputFile=fig2_compressed.pdf fig2.pdf
+gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -sOutputFile=figure_scripts/fig2_compressed.pdf figure_scripts/fig2.pdf
