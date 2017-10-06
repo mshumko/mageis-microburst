7763cb729894adbdee325dff686d343a52936979
Mike Shumko <msshumko@gmail.com>
2017-10-06 17:33:07 -0600

    Keep working on the bounce period plots. Added the MagEIS flux and pitch angle evolution.

--- bounce_period.py
+++ bounce_period.py
@@ -1,9 +1,11 @@
 # This script calculates the return time.
 import numpy as np
-from datetime import datetime
+from datetime import datetime, timedelta
 #import scipy.optimize
 import matplotlib.pyplot as plt
+import matplotlib.dates as md
 import matplotlib.gridspec as gridspec
+plt.rcParams.update({'font.size': 12})
 
 import sys
 sys.path.insert(0, '/home/mike/research/mission-tools/vap')
@@ -15,12 +17,11 @@ import IRBEM
 # RBSP-A location at the time of the microburst
 X = {'x1':30409, 'x2':-8.8, 'x3':115.1, 'dateTime':datetime(2017, 3, 31, 11, 18)}
 maginput = {'Kp':40}
-#tReturn(X, maginput, 320)
 
 model = IRBEM.MagFields()
 
 alphaArr = np.arange(10, 90, 5)
-Earr = np.arange(30, 200, 5)
+Earr = np.arange(30, 100, 5)
 aa, ee = np.meshgrid(alphaArr, Earr) # Meshgrid for plotting
 tArr = np.nan*np.ones((len(Earr), len(alphaArr))) # Bounce period array.
 
@@ -28,21 +29,25 @@ for i_a, a in enumerate(alphaArr):
     tArr[:, i_a] = model.bounce_period(X, maginput, Earr, alpha = a)
     
 # Plot the bounce periods
-fig = plt.figure(figsize=(15, 10), dpi=80, facecolor='white')
-gs = gridspec.GridSpec(1,1)
-ax = fig.add_subplot(gs[0, 0], facecolor='k')
+fig = plt.figure(figsize=(15, 8), dpi=80, facecolor='white')
+gs = gridspec.GridSpec(2,2)
+ax = fig.add_subplot(gs[:, 0], facecolor='k')
 p = ax.pcolormesh(ee, aa, tArr)
+CS = ax.contour(ee, aa, tArr, 6, colors='w')
+ax.clabel(CS, inline=1)
 plt.colorbar(p, label='Bounce period (s)')
 ax.set(xlabel='Energy (keV)', ylabel=r'$\alpha_{sc}$', 
     title='Electron bounce period in the T89 model, Kp = {}'.format(
     maginput['Kp']/10))
-#plt.show()
+
+# Draw boxes in E-alpha space for the microbursts
 
 ### PLOT the HighRate data ###
-fig2 = plt.figure(figsize=(15, 10), dpi=80, facecolor='white')
-gs = gridspec.GridSpec(2,1)
-bx = fig2.add_subplot(gs[0, 0])
-cx = fig2.add_subplot(gs[1, 0], sharex=bx)
+#fig2 = plt.figure(figsize=(15, 10), dpi=80, facecolor='white')
+#gs = gridspec.GridSpec(2,1)
+bx = fig.add_subplot(gs[0, 1])
+cx = fig.add_subplot(gs[1, 1], sharex=bx)
+#dx = fig2.add_subplot(gs[3, 0], sharex=bx)
 
 tKey = 'muBurst'
 date = datetime(2017, 3, 31)
@@ -60,74 +65,47 @@ fluxObj = plot_mageis.magEISspectra(rb_id, date, dataLevel = 3)
 fluxObj.tBounds = times[tKey]
 fluxObj.loadMagEIS(instrument='LOW', highrate=True)
 fluxObj.plotHighRateTimeSeries(smooth=10, ax=bx)
-bx.set_ylim(bottom=5*10**2)
-bx.set(title='MagEIS timeseries from {}'.format(date.isoformat()))
 
-print(fluxObj.magEISdata[fluxObj.alphaKey].shape)
-print(fluxObj.times.shape)
+### NOW PLOT PITCH ANGLES ###
 validIda = np.where(fluxObj.magEISdata[fluxObj.alphaKey] != -1E31)
-cx.fill_between(fluxObj.times[validIda], 
-    fluxObj.magEISdata[fluxObj.alphaKey][validIda] + 10,
-    fluxObj.magEISdata[fluxObj.alphaKey][validIda] - 10)
-cx.plot(fluxObj.times[validIda], fluxObj.magEISdata[fluxObj.alphaKey][validIda], 
-    'r')
+alpha = fluxObj.magEISdata[fluxObj.alphaKey][validIda]
 
+# Now map alpha into a 180 degree space
+flipAlpha = np.where(alpha > 180)[0]
+alpha[flipAlpha] = -alpha[flipAlpha] % 360
+
+cx.fill_between(fluxObj.times[validIda], alpha + 10, alpha - 10, color='b')
+cx.plot(fluxObj.times[validIda], alpha, 'r')
+    
+cx.set(ylim=(0, 60), xlim=(datetime(2017, 3, 31, 11, 17, 8), 
+                    datetime(2017, 3, 31, 11, 17, 13)) )
+# Draw guides
+t = [None]*4
+t[0] = datetime(2017, 3, 31, 11, 17, 9, int(78E4))
+t[1] = datetime(2017, 3, 31, 11, 17, 10, int(24E4))
+t[2] = datetime(2017, 3, 31, 11, 17, 10, int(85E4))
+t[3] = datetime(2017, 3, 31, 11, 17, 11, int(48E4))
+
+tt = [None]*4
+for i in range(4):
+    tt[i] = md.date2num(t[i])
+
+for a in [bx, cx]:    
+    a.axvline(x=t[0], c='k')
+    a.axvline(x=t[1], c='k')
+    a.axvline(x=t[2], c='k')
+    a.axvline(x=t[3], c='k')
+
+plt.setp(bx.get_xticklabels(), visible=False)
+
+bx.set_ylim(bottom=5*10**2)
+bx.set(title='MagEIS LOW timeseries from {}'.format(date.isoformat()))
+bx.legend()
+bx.set_ylabel(r'Flux $(keV \ cm^2 \ sr \ s)^{-1}$')
+cx.set_title('MagEIS LOW pitch angle evolution')
+cx.set_ylabel(r'$\alpha_{eq}$')
+
+#dx.set_ylabel(r'$\alpha_{eq}$')
+cx.set_xlabel('UTC')
+gs.tight_layout(fig)
 plt.show()
-
-
-# Now calculate the 
-
-###R_e = 6.371E6 # meters
-
-###beta = lambda Ek, Erest = 511: np.sqrt(1-((Ek/Erest)+1)**(-2)) # Ek,a dErest must
-###vparalel = lambda Ek, Bm, B, alpha:c*beta(Ek)*np.sqrt(
-###    1 - np.sin(np.deg2rad(alpha))*np.abs(B/Bm) )
-    
-    
-###def tReturn(X, maginput, alpha, hemi='same'):
-###    """
-###    
-###    """
-###    model = IRBEM.MagFields()
-###    # Trace field line
-###    fLine = model._interpolate_field_line(X, maginput)
-###    
-###    B_m = fLine['inputB']/np.sin(np.deg2rad(alpha))**2 # Mirror point |B|
-###    
-###    # Tranformed coordinates of the sc location.
-###    coords = IRBEM.Coords()
-###    scLoc = coords.coords_transform(X['dateTime'], 
-###        [X['x1'], X['x2'], X['x3']], 0, 1)
-###    
-###    if scLoc[0, 2] > 0:
-###        # Find index (and location of spacecraft)
-###        S_sc = scipy.optimize.brentq(fLine['fB'], 0, len(fLine['S'])/2)
-###        
-###        # Find the index of the mirror point.
-###        if hemi == 'same':
-###            S_m = scipy.optimize.brentq(fLine['fB']+fLine['inputB']-B_m, 0, 
-###                len(fLine['S'])/2)
-###    else:
-###        S_sc = scipy.optimize.brentq(fLine['fB'], len(fLine['S'])/2, 
-###                                             len(fLine['S'])-1)
-###        # Find the index of the mirror point.                                    
-###        if hemi == 'same':
-###            S_m = scipy.optimize.brentq(fLine['fB']+fLine['inputB']-B_m, 
-###                len(fLine['S'])/2, len(fLine['S'])-1)
-###                
-###    #plt.plot(fLine['fx'](fLine['S']), fLine['fz'](fLine['S']))
-###    plt.scatter(fLine['fx'](fLine['S']), fLine['fz'](fLine['S']), 
-###        c=fLine['fB'](fLine['S'])+fLine['inputB'], norm=matplotlib.colors.LogNorm())
-###    #plt.scatter(scLoc[0, 0], scLoc[0, -1], c='r', marker='*', s=500)
-###    plt.scatter(fLine['fx'](S_sc), fLine['fz'](S_sc), c='r', marker='*', s=500)
-###    plt.scatter(fLine['fx'](S_m), fLine['fz'](S_m), c='b', marker='*', s=500)
-###    plt.show()
-###    return 
-
-###model = IRBEM.MagFields()
-###fLine = model._interpolate_field_line(X, maginput)
-###B_m = fLine['inputB']/np.sin(a)**2
-
-###t = [2*np.sum(np.divide(ds[1:-1], vparalel(Ei, fLine['inputB'], dB, 
-###                                              Erest = Erest)[1:-1])) for Ei in E]
-
