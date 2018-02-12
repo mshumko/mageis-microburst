10a2f30714c8b86ef92d82a5785c404806e6f494
mshumko <msshumko@gmail.com>
2018-02-12 12:10:51 -0700

    Adjusted the energy channels to be plotted and put the plotting code into a loop over pitch angles to automatically generate all of the plots.

--- rabbit_holes/plot_hope_psd.py
+++ rabbit_holes/plot_hope_psd.py
@@ -11,9 +11,9 @@ from spacepy import pycdf
 sys.path.insert(0, '/home/mike/research/mageis-microburst')
 import calc_mageis_psd
 
-START_TIME = datetime(2017, 3, 31, 11, 15, 0)
+START_TIME = datetime(2017, 3, 31, 11, 16, 0)
 END_TIME = datetime(2017, 3, 31, 11, 18, 0)
-START_ENERGY_IND = 55
+START_ENERGY_IND = 50
 END_ENERGY_IND = 70
 PA_PLOT_IND = 2
 
@@ -34,25 +34,30 @@ d['HOPE_ENERGY_Ele'] = d['HOPE_ENERGY_Ele'][validIdt, :]
 d['e_energy_ch'] = [statistics.mode(i) for i in np.transpose(d['HOPE_ENERGY_Ele'])]
 # Calculate PSD
 
-#for a in range(11):
-d['FEDU_PSD'] = np.nan*np.ones_like(d['HOPE_ENERGY_Ele'])
-for i, (j, Ek) in enumerate(zip(np.transpose(d['FEDU'][:, PA_PLOT_IND, :]), d['e_energy_ch'])):
-    d['FEDU_PSD'][:, i] = 1E9*calc_mageis_psd.PhaseSpaceDensity.f(j, Ek/1000)
+for a in range(11):
+    d['FEDU_PSD'] = np.nan*np.ones_like(d['HOPE_ENERGY_Ele'])
+    for i, (j, Ek) in enumerate(zip(np.transpose(d['FEDU'][:, a, :]), d['e_energy_ch'])):
+        d['FEDU_PSD'][:, i] = 1E9*calc_mageis_psd.PhaseSpaceDensity.f(j, Ek/1000)
 
-# Plot the PSD
-colors = cm.rainbow(np.linspace(0, 1, END_ENERGY_IND - START_ENERGY_IND))
-for (i, ii) in enumerate(range(START_ENERGY_IND, END_ENERGY_IND)):
-    plt.scatter(d['Epoch_Ele'], d['FEDU_PSD'][:, ii], 
-                label='{}'.format(round(d['e_energy_ch'][ii]/1000)), c=colors[i])
-plt.legend(loc=1, bbox_to_anchor=(1.15, 1.05), title='Energy [keV]')
-plt.yscale('log')
-plt.xlabel('UTC')
-plt.ylabel(r'PSD $c^3/(cm \ MeV)^3$')
-plt.xlim(START_TIME, END_TIME)
-
-plt.ylim(bottom=10**-2, top=10)
-plt.title(r'2017-03-31 | RBSP-A | HOPE Electrons | $\alpha_L$' + 
-            ' = {}'.format(d['PITCH_ANGLE'][PA_PLOT_IND]))
-plt.savefig(('/home/mike/Dropbox/0_grad_work/mageis_microburst/plots/'
-            'HOPE/20170331_hope_psd_a_{}.png'.format(int(d['PITCH_ANGLE'][PA_PLOT_IND]))))
-plt.clear()
+    # Plot the PSD
+    fig, ax = plt.subplots()
+    ax.axhline(0.1)
+    colors = cm.rainbow(np.linspace(0, 1, END_ENERGY_IND - START_ENERGY_IND))
+    for (i, ii) in enumerate(range(START_ENERGY_IND, END_ENERGY_IND)):
+        ax.scatter(d['Epoch_Ele'], d['FEDU_PSD'][:, ii], 
+                    label='{}'.format(round(d['e_energy_ch'][ii]/1000)), c=colors[i])
+    ax.legend(loc=1, bbox_to_anchor=(1.15, 1.1), title='Energy [keV]',
+                fontsize=10)
+    
+    ax.set_yscale('log')
+    ax.set_xlabel('UTC')
+    ax.set_ylabel(r'PSD $c^3/(cm \ MeV)^3$')
+    ax.set_xlim(START_TIME, END_TIME)
+    ax.set_ylim(bottom=10**-2, top=10)
+    fig.autofmt_xdate()
+    
+    ax.set_title(r'2017-03-31 | RBSP-A | HOPE Electrons | $\alpha_L$' + 
+                ' = {}'.format(d['PITCH_ANGLE'][a]))
+    plt.savefig(('/home/mike/Dropbox/0_grad_work/mageis_microburst/plots/'
+                'HOPE/20170331_hope_psd_a_{}.png'.format(int(d['PITCH_ANGLE'][a]))))
+    plt.close()
